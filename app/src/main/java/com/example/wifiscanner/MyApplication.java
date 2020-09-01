acb57999fb776be407be3519fca491fb8b418bac
icecoobe <icecoobe@qq.com>
2020-09-01 18:31:03 +0800

    initial commit

---
+++ app/src/main/java/com/example/wifiscanner/MyApplication.java
@@ -0,0 +1,17 @@
+package com.example.wifiscanner;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+    private static MyApplication instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+    public static MyApplication getInstance(){
+        return instance;
+    }
+}
